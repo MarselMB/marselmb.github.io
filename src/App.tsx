4595fa6bda23baf6a041a3a5ae460198ccd00e71
Буляккулов Марсель Маратович <m.bulyakkulov@s7.ru>
2024-03-05 00:47:29 +0300

    add info about me

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,42 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+
+        <div className="App-header-text">
+          <div className="App-header-text-name">Буляккулов Марсель</div>
+          <div className="App-header-text-city">г. Москва</div>
+        </div>
       </header>
+
+      <main className="App-main">
+        <div>
+          <h3>Цели</h3>
+
+          <p>
+            <ul>
+              <li>Научиться продвинутым возможностям Redux, Redux-Saga, Redux-thunk</li>
+              <li>Создавать SPA-приложения и оптимизировать их для production</li>
+              <li>Изучить TypeScript: писать чистый и лаконичный код, применяя эту технологию</li>
+              <li>Освоить интеграционные и юнит-тесты</li>
+              <li>Изучить GraphQL, Apollo, Relay</li>
+            </ul>
+
+            <div>
+              Также хочу узнать, как работает под капотом Webpack и Babel, освоить паттерны функционального
+              программирования и научиться их применять в React
+            </div>
+
+            <div>
+              <h3>Обо мне</h3>
+
+              <p>
+                Опыт в разработке - 1 год. За это время участвовал в разработке двух приложений цифровых двойников
+                технических установок. Стек: React + TS, Redux, MaterialUI.
+              </p>
+            </div>
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
