ecf13a15906c3867feca284cab071001730e1e64
Буляккулов Марсель Маратович <m.bulyakkulov@s7.ru>
2024-03-15 11:20:12 +0300

    add types

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -43,14 +43,87 @@
  * - type ('Profit')
  * */
 
+import { v4 as uuidv4 } from 'uuid';
+import { faker } from '@faker-js/faker';
+
+interface ICategory {
+  id: string;
+  name: string;
+  photo?: string;
+}
+
+interface IProduct extends Required<ICategory> {
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: ICategory;
+}
+
+interface IOperation extends Omit<ICategory, 'photo'> {
+  desc?: string;
+  createdAt: string;
+  amount: string;
+  category: ICategory;
+  type: 'Cost' | 'Profit';
+}
+
+interface IProductName {
+  manufacturer: string;
+  model: string;
+}
+
+const getRandomPhoto = (category: string): string => faker.image.urlLoremFlickr({ category });
+
+const getRandomPrice = (): number => +faker.commerce.price({ min: 1000, max: 10000, dec: 0 });
+
+const getRandomProductName = (): IProductName => ({
+  manufacturer: faker.vehicle.manufacturer(),
+  model: faker.vehicle.model(),
+});
+
+const getRandomDesc = () =>
+  `fuel type: ${faker.vehicle.fuel()}, color: ${faker.vehicle.color()}, vehicle type: ${faker.vehicle.type()}`;
+
+const getCategoryProduct = (manufacturer: string): ICategory => ({
+  id: uuidv4(),
+  name: manufacturer,
+  photo: getRandomPhoto(manufacturer),
+});
+
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+export const createRandomProduct = (createdAt: string): IProduct => {
+  const { manufacturer, model } = getRandomProductName();
+
+  return {
+    id: uuidv4(),
+    name: `${manufacturer} ${model}`,
+    photo: getRandomPhoto('car'),
+    desc: faker.datatype.boolean() ? getRandomDesc() : undefined,
+    createdAt,
+    oldPrice: faker.datatype.boolean() ? getRandomPrice() : undefined,
+    price: getRandomPrice(),
+    category: getCategoryProduct(manufacturer),
+  };
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): IOperation => {
+  const { manufacturer, model } = getRandomProductName();
+
+  return {
+    id: uuidv4(),
+    name: `${manufacturer} ${model}`,
+    desc: faker.datatype.boolean() ? getRandomDesc() : undefined,
+    createdAt,
+    amount: `${getRandomPrice()}`,
+    category: getCategoryProduct(manufacturer),
+    type: faker.datatype.boolean() ? 'Cost' : 'Profit',
+  };
+};
