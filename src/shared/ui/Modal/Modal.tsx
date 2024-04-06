5ae05c02376330fe35cd2e457b33a979770ff1d5
Буляккулов Марсель Маратович <m.bulyakkulov@s7.ru>
2024-04-06 12:12:04 +0300

    added onClose to modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC, ReactNode, useEffect, useState } from 'react';
 import close from './close.png';
 import styles from './Modal.module.scss';
 
@@ -8,18 +8,35 @@ interface ModalProps {
 }
 
 const Modal: FC<ModalProps> = ({ visible, children }) => {
-  if (!visible) {
-    return null;
-  }
+  const [isShow, setIsShow] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsShow(visible);
+  }, [visible]);
+
+  const handleOnClose = () => {
+    setIsShow(false);
+  };
+
+  const handleOnClickModal = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (event.target === event.currentTarget) {
+      handleOnClose();
+    }
+  };
+
   return (
-    <div className={styles.modal}>
-      <div className={styles.modalContent}>
-        {children}
-        <button className={styles.closeButton}>
-          <img src={close} alt="close" className={styles.img} />
-        </button>
-      </div>
-    </div>
+    <>
+      {isShow && (
+        <div className={styles.modal} onClick={handleOnClickModal}>
+          <div className={styles.modalContent}>
+            {children}
+            <button className={styles.closeButton} onClick={handleOnClose}>
+              <img src={close} alt="close" className={styles.img} />
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
